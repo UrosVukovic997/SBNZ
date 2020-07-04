2debbfd40f7bd9a87a48d7f66f92dab4ee56d1e7
Uroš Vuković <57104944+UrosVukovic997@users.noreply.github.com>
2020-07-04 19:20:46 +0200

    Controller update

--- drools-spring-app/src/main/java/sbnz/integracija/example/controller/MerneStaniceController.java
+++ drools-spring-app/src/main/java/sbnz/integracija/example/controller/MerneStaniceController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -42,7 +43,15 @@ public class MerneStaniceController {
 	
 	@RequestMapping(value = "/dodaj", method = RequestMethod.POST, consumes = "application/json")
 	public void dodaj(@RequestBody MernaStanicaDTO mernaStanicaDTO) {
-		MernaStanica mernaStanica = mernaStanicaService.findById(mernaStanicaDTO.getRedniBroj());
+		MernaStanica mernaStanica = new MernaStanica(mernaStanicaDTO.getRedniBroj(), mernaStanicaDTO.getNaziv(), mernaStanicaDTO.getLokacija(),
+				mernaStanicaDTO.getNivoVode(), mernaStanicaDTO.getMaxNivo(), mernaStanicaDTO.getPadavine(), true);
+		mernaStanicaService.save(mernaStanica);
+		
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT, consumes = "application/json")
+	public void update(@PathVariable("id") Integer id, @RequestBody MernaStanicaDTO mernaStanicaDTO) {
+		MernaStanica mernaStanica = mernaStanicaService.findById(id);
 		mernaStanica.setNivoVode(mernaStanicaDTO.getNivoVode());
 		mernaStanica.setPadavine(mernaStanicaDTO.getPadavine());
 		mernaStanicaService.save(mernaStanica);
